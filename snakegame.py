6d6552094463fbd80aa8ef5058abd6482f451a9b
anonymous <anonymous@email.com>
2026-05-04 10:10:31 +0200

    VIDEO 3

--- snakegame.py
+++ snakegame.py
@@ -2,8 +2,14 @@ import pygame
 pygame.init()
 
 WIN_SIZE = 800 
+SQUARE_COUNT = 20 # Anzahl 20 Squares für Raster implementieren (3)
+SQUARE_SIZE = WIN_SIZE / SQUARE_COUNT # Square einzeln definiert durch Gesamtgröße : Anzahl der Squares (3)
+
 screen = pygame.display.set_mode((WIN_SIZE, WIN_SIZE)) #erstellt ein Fenster mit den angegebenen Maßen
 pygame.display.set_caption("Snake Game") #setzt den Titel des Fensters
+
+head_column = 0 #Position Kopf ( Reihe 0) (3)
+head_row = 0
 
 x = 50
 y= 70
@@ -31,7 +37,25 @@ while run:
         y += 2
 
     screen.fill((120,0,0)) #füllt den Bildschirm mit der Farbe schwarz (RGB Werte)
-    pygame.draw.rect(screen, (0,255,0), (x,y,50,50)) #zeichnet ein Rechteck auf den Bildschirm (Farbe, Position und Größe)
+
+
+    # Video 3 der Playlist
+
+    #Raster Programmieren (3)
+    screen.fill((0, 0, 0))  # schwarzer Hintergrund (3)
+
+    head_x = head_column * SQUARE_SIZE 
+    head_y=head_row* SQUARE_SIZE
+    pygame.draw.rect(screen, (0, 255, 0), (head_x , head_y, SQUARE_SIZE, SQUARE_SIZE )) #zeichnet ein Rechteck auf den Bildschirm (Farbe, Position und Größe) + angepasst an das Raster
+
+    for i in range(SQUARE_COUNT) : # Linien als Raster (3)
+        line_pos = SQUARE_SIZE * i
+        pygame.draw.line(screen, (255, 255, 255), (line_pos, 0), (line_pos, WIN_SIZE), 2 ) # Linienfarbe weiß + Startposition / Endposition +  Liniendicke ( Senkrecht) (3) 
+        pygame.draw.line(screen, (255, 255, 255), (0 , line_pos), (WIN_SIZE , line_pos), 2 ) # Horizontal
+
+
+
+    
     pygame.display.update() #aktualisiert den Inhalt des Fensters
 
 pygame.display.quit() #schließt das fenster 
